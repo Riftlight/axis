9a062bcb91780a3959d4258e446af6a8539acb47
Riftlight <riftlighting@gmail.com>
2026-07-01 01:11:16 -0600

    Levels folder and level switching system

---
+++ LevelManager.cs
@@ -0,0 +1,38 @@
+// autoload, not tied to any Node
+using Godot;
+
+public partial class LevelManager : Node
+{
+    public static LevelManager Instance { get; private set; }
+
+    private readonly string[] _levels =
+    {
+        "res://Levels/Level1.tscn"
+    };
+
+    private int _currentIndex = 0;
+
+    public override void _Ready()
+    {
+        Instance = this;
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        _currentIndex++;
+        _currentIndex %= _levels.Length;
+        GetTree().ChangeSceneToFile(_levels[_currentIndex]);
+    }
+
+    public void RestartLevel()
+    {
+        GetTree().ChangeSceneToFile(_levels[_currentIndex]);
+    }
+
+    public void LoadLevel(int index)
+    {
+        _currentIndex = Mathf.Clamp(index, 0, _levels.Length - 1);
+        GetTree().ChangeSceneToFile(_levels[_currentIndex]);
+    }
+}
