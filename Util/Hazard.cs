f18c90d92ffdec3b224cb5f3d3f98a179df4928c
Riftlight <riftlighting@gmail.com>
2026-07-04 22:33:44 -0600

    Add Hazard blocks

---
+++ Util/Hazard.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Hazard : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		BodyEntered += OnBodyEntered;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is Player player)
+		{
+			LevelManager.Instance.RestartLevel();
+		}
+	}
+}
