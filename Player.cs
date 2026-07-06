5e481e02d1bcfb5533dded3ee71014adb2107842
Riftlight <riftlighting@gmail.com>
2026-07-05 22:53:00 -0600

    Add player sprite and implement stretching and squashing

--- Player.cs
+++ Player.cs
@@ -4,15 +4,31 @@ public partial class Player : CharacterBody2D
 {
 	[Export] public float GravityStrength = 1400f; // per second
 	[Export] public float Friction = 800f;
-	[Export] public float MaxSpeed = 1000f;
+	[Export] public float MaxSpeed = 1500f;
 	[Export] public Vector2 BodySize = new Vector2(28, 28);
 
 	private Vector2 _gravityDir = Vector2.Down;
+
+	private Sprite2D _sprite;
+	private float _spriteSize;
+	private Vector2 _collCenter;
+	private Vector2 _collHalfSize;
+
+	private float _squishY = 1f;
+	private Tween _squishTween;
+	private bool _wasGrounded = false;
 
 	public override void _Ready()
 	{
 		this.TopLevel = true; // render over other things, mostly for arrow
 		this.UpDirection = -_gravityDir;
+
+		_sprite = GetNode<Sprite2D>("Sprite2D"); // todo shift to exported property
+		_spriteSize = _sprite.Scale.X; // should always be a square
+
+		CollisionShape2D collShape = GetNode<CollisionShape2D>("CollisionShape2D");
+		_collCenter = collShape.Position;
+		_collHalfSize = ((RectangleShape2D)collShape.Shape).Size / 2f;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -30,6 +46,7 @@ public partial class Player : CharacterBody2D
 			Velocity += surfaceDir * (lessened - lateralSpeed);
 		}
 
+		// Break breakables
 		Vector2 preSlideVelocity = Velocity;
 		MoveAndSlide();
 		for (int i = 0; i < GetSlideCollisionCount(); i++)
@@ -38,11 +55,42 @@ public partial class Player : CharacterBody2D
 			if (coll.GetCollider() is Breakable breakable)
 				breakable.PlayerHit(preSlideVelocity, coll.GetNormal());
 		}
+
+		bool grounded = IsGrounded();
+		if (grounded && !_wasGrounded)
+			LandingSquish(preSlideVelocity.Dot(_gravityDir));
+		_wasGrounded = grounded;
 	}
 
 	public override void _Process(double delta)
 	{
+		UpdateSprite();
 		QueueRedraw(); // todo
+	}
+
+	private void UpdateSprite()
+	{
+		// rotate the slime so its bottom is in the current down direction
+		_sprite.Rotation = _gravityDir.Angle() - Mathf.Pi / 2f;
+
+		Vector2 lateralDir = new Vector2(-_gravityDir.Y, _gravityDir.X);
+		float fallSpeed = Velocity.Dot(_gravityDir);
+		float lateralSpeed = Velocity.Dot(lateralDir);
+
+		float yScale =
+			(1f
+			+ Mathf.Clamp(fallSpeed/MaxSpeed, -0.2f, 1f)
+			- Mathf.Clamp(Mathf.Abs(lateralSpeed) / MaxSpeed, 0f, 0.15f))
+			* _squishY;
+		float xScale = 1f / yScale;
+
+		_sprite.Scale = new Vector2(_spriteSize * xScale, _spriteSize * yScale);
+
+		float halfExtent = _collHalfSize.Dot(_gravityDir.Abs());
+		Vector2 floorEdge = _collCenter + _gravityDir * halfExtent;
+		
+		_sprite.Position = floorEdge - _gravityDir * ((_spriteSize/2*16) * yScale);
+
 	}
 
 	private bool IsGrounded()
@@ -51,6 +99,18 @@ public partial class Player : CharacterBody2D
 			if (GetSlideCollision(i).GetNormal().Dot(-_gravityDir) > 0.7f) // 0.7 is approx cos(45deg), could lower threshold but should work fine on everything AA
 				return true;
 		return false;
+	}
+
+	private void LandingSquish(float impactSpeed)
+	{
+		float t = Mathf.Clamp(impactSpeed / MaxSpeed, 0f, 1f);
+		_squishY = Mathf.Lerp(1f, 0.35f, t);
+
+		_squishTween?.Kill();
+		_squishTween = CreateTween();
+		_squishTween.SetEase(Tween.EaseType.Out);
+		_squishTween.SetTrans(Tween.TransitionType.Elastic);
+		_squishTween.TweenMethod(Callable.From<float>(v => _squishY = v), _squishY, 1f, 0.5f);
 	}
 
 	public override void _Input(InputEvent @event)
@@ -67,7 +127,7 @@ public partial class Player : CharacterBody2D
 		if (newDir == _gravityDir) return;
 
 		_gravityDir = newDir;
-		UpDirection = _gravityDir;
+		UpDirection = -_gravityDir;
 	}
 
 	private Vector2 GetTargetGravityDir()
@@ -83,7 +143,7 @@ public partial class Player : CharacterBody2D
 	public override void _Draw()
 	{
 		// player body
-		DrawRect(new Rect2(-BodySize / 2.0f, BodySize), new Color(0.3f, 0.75f, 1f));
+		// DrawRect(new Rect2(-BodySize / 2.0f, BodySize), new Color(0.3f, 0.75f, 1f));
 
 		// gravity arrow
 		Vector2 dir = GetTargetGravityDir();
