2a8b1c272102b3340f8f06387e7c5a2829394c96
Riftlight <riftlighting@gmail.com>
2026-07-12 21:20:53 -0500

    Change texture to show restart when at 0 remaining switches

--- Util/SwitchControl/SwitchUi.cs
+++ Util/SwitchControl/SwitchUi.cs
@@ -4,16 +4,28 @@ public partial class SwitchUi : CanvasLayer
 {
 	[Export] public bool SwitchesAreLimited = true;
 	private Label label;
+	private Sprite2D sprite;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		if (SwitchesAreLimited)
+		if (SwitchesAreLimited) {
 			label = GetNode<Label>("Label");
+			sprite = GetNode<Sprite2D>("Sprite2D");
+		}
 	}
 
 	public void UpdateSwitches(int remaining)
 	{
-		if (SwitchesAreLimited)
-			label.Text = ""+remaining;
+		if (!SwitchesAreLimited) return;
+
+		label.Text = ""+remaining;
+		if (remaining == 0) {
+			sprite.Texture = ImageTexture.CreateFromImage(Image.LoadFromFile("res://Textures/switchrestart.png"));
+			label.Visible = false;
+		} else
+		{
+			sprite.Texture = ImageTexture.CreateFromImage(Image.LoadFromFile("res://Textures/switchcounter.png"));
+			label.Visible = true;
+		}
 	}
 }
