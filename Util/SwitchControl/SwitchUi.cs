4635fa33f657426bd683089881f227e071959ff2
Riftlight <riftlighting@gmail.com>
2026-07-13 18:16:08 -0500

    Don't create new ImageTexture and Image on every UpdateSwitches

--- Util/SwitchControl/SwitchUi.cs
+++ Util/SwitchControl/SwitchUi.cs
@@ -5,12 +5,16 @@ public partial class SwitchUi : CanvasLayer
 	[Export] public bool SwitchesAreLimited = true;
 	private Label label;
 	private Sprite2D sprite;
+	private ImageTexture counter, restart;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		if (SwitchesAreLimited) {
 			label = GetNode<Label>("Label");
 			sprite = GetNode<Sprite2D>("Sprite2D");
+
+			counter = ImageTexture.CreateFromImage(Image.LoadFromFile("res://Textures/switchcounter.png"));
+			restart = ImageTexture.CreateFromImage(Image.LoadFromFile("res://Textures/switchrestart.png"));
 		}
 	}
 
@@ -20,11 +24,12 @@ public partial class SwitchUi : CanvasLayer
 
 		label.Text = ""+remaining;
 		if (remaining == 0) {
-			sprite.Texture = ImageTexture.CreateFromImage(Image.LoadFromFile("res://Textures/switchrestart.png"));
+			sprite.Texture = restart;
 			label.Visible = false;
-		} else
+		} 
+		else
 		{
-			sprite.Texture = ImageTexture.CreateFromImage(Image.LoadFromFile("res://Textures/switchcounter.png"));
+			sprite.Texture = counter;
 			label.Visible = true;
 		}
 	}
