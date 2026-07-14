62926814c5e18c9a0faba66338c97e9d28b780ca
Riftlight <riftlighting@gmail.com>
2026-07-13 23:21:05 -0500

    Add Breakable texture (tiling dysfunctional)

--- Util/Breakable.cs
+++ Util/Breakable.cs
@@ -4,12 +4,14 @@ public partial class Breakable : StaticBody2D
 {
 
 	[Export] public float SpeedThreshold = 1000f;
+	[Export] public Texture2D BreakableTexture;
 
 	private Vector2 _downVector;
 	private CollisionShape2D coll;
 
 	public override void _Ready()
 	{
+		this.TextureRepeat = TextureRepeatEnum.Enabled; // tile
 		coll = GetNode<CollisionShape2D>("CollisionShape2D"); // should probably be replaced with [Export]ed property
 
 		Vector2 rotatedDown = Vector2.Down.Rotated(Rotation);
@@ -29,4 +31,17 @@ public partial class Breakable : StaticBody2D
 			this.coll.SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
 		}
 	}
+
+	public override void _Draw()
+	{
+		if (BreakableTexture == null) return;
+
+		CollisionShape2D coll = GetNodeOrNull<CollisionShape2D>("CollisionShape2D");
+		if (coll?.Shape is not RectangleShape2D rect) return; // this will probably happen later, doesnt happen rn
+
+		DrawSetTransform(Vector2.Zero, 0f, Vector2.One / Scale);
+		DrawTextureRect(BreakableTexture, new Rect2(-rect.Size / 2f, rect.Size), true);
+		DrawSetTransform(Vector2.Zero, 0f);
+	}
+
 }
